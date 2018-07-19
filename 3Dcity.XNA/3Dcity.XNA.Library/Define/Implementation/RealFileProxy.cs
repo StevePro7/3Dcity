f5d20d0e0a1d2f11574c631d65e9ff9bdb928ea6
StevePro7 <steveprozune@hotmail.com>
2018-07-19 20:59:46 +0100

    Update define directory

---
+++ 3Dcity.XNA/3Dcity.XNA.Library/Define/Implementation/RealFileProxy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using WindowsGame.Define.Interfaces;
+using Microsoft.Xna.Framework;
+
+namespace WindowsGame.Define.Implementation
+{
+	public class RealFileProxy : IFileProxy
+	{
+		public Stream GetStream(String path)
+		{
+			return TitleContainer.OpenStream(path);
+		}
+	}
+}
---
+++ 3Dcity.XNA/3Dcity.XNA.Library/Define/Interfaces/IScreen.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+
+namespace WindowsGame.Define.Interfaces
+{
+	public interface IScreen
+	{
+		void Initialize();
+		void LoadContent();
+		String Update(GameTime gameTime);
+		void Draw();
+	}
+}
