3e0478eb7fb32b6ff901d81857f5a541921c3914
StevePro7 <steveprozune@hotmail.com>
2018-07-19 22:06:16 +0100

    Update game configuration

--- 3Dcity.XNA/3Dcity.XNA.Library/Common/MyGame.cs
+++ 3Dcity.XNA/3Dcity.XNA.Library/Common/MyGame.cs
@@ -1,3 +1,4 @@
+using WindowsGame.Common.Static;
 using Microsoft.Xna.Framework;
 using WindowsGame.Common.TheGame;
 using WindowsGame.Define;
@@ -14,14 +15,25 @@ namespace WindowsGame.Common
 
 		public static void Initialize()
 		{
+			Manager.Logger.Initialize();
+			Manager.RandomManager.Initialize();
+
+			Manager.ConfigManager.Initialize();
+
+			Manager.ResolutionManager.Initialize();
+			Manager.ScreenManager.Initialize();
+			Manager.SoundManager.Initialize();
+			Manager.SpriteManager.Initialize();
+			Manager.StorageManager.Initialize();
+			Manager.ThreadManager.Initialize();
 		}
 
 		public static void LoadContent()
 		{
-			//Engine.Game.IsFixedTimeStep = Constants.IsFixedTimeStep;
-			//Engine.Game.TargetElapsedTime = TimeSpan.FromSeconds(1.0f / Constants.FramesPerSecond);
-			//Engine.Game.IsMouseVisible = Constants.IsMouseVisible;
-			//Manager.ResolutionManager.LoadContent(Constants.IsFullScreen, Constants.ScreenWide, Constants.ScreenHigh, Constants.UseExposed, Constants.ExposeWide, Constants.ExposeHigh);
+			Engine.Game.IsFixedTimeStep = Constants.IsFixedTimeStep;
+			Engine.Game.TargetElapsedTime = TimeSpan.FromSeconds(1.0f / Constants.FramesPerSecond);
+			Engine.Game.IsMouseVisible = Constants.IsMouseVisible;
+			Manager.ResolutionManager.LoadContent(Constants.IsFullScreen, Constants.ScreenWide, Constants.ScreenHigh, Constants.UseExposed, Constants.ExposeWide, Constants.ExposeHigh);
 		}
 
 		public static void LoadContentAsync()
@@ -36,6 +48,18 @@ namespace WindowsGame.Common
 
 		public static void Update(GameTime gameTime)
 		{
+			Manager.InputManager.Update(gameTime);
+
+#if WINDOWS
+			Boolean escape = Manager.InputManager.Escape();
+			if (escape)
+			{
+				Engine.Game.Exit();
+				return;
+			}
+#endif
+
+			Manager.ScreenManager.Update(gameTime);
 		}
 
 		public static void Draw()
--- 3Dcity.XNA/3Dcity.XNA.Library/Common/Static/Constants.cs
+++ 3Dcity.XNA/3Dcity.XNA.Library/Common/Static/Constants.cs
@@ -5,5 +5,22 @@ namespace WindowsGame.Common.Static
 	public static class Constants
 	{
 		public const String CONTENT_DIRECTORY = "Content";
+
+		// Global data.
+		public const Boolean IsFixedTimeStep = true;
+		public const UInt32 FramesPerSecond = 100;
+
+		// Custom data.
+		public const Boolean IsFullScreen = false;
+		public const Boolean IsMouseVisible = true;
+
+		public const UInt16 ScreenWide = 640;
+		public const UInt16 ScreenHigh = 640;
+
+		public const Boolean UseExposed = true;
+		public const UInt16 ExposeWide = 640;
+		public const UInt16 ExposeHigh = 640;
+
+		public const Byte GameOffsetX = 0;
 	}
 }
