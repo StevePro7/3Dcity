6eeb39ab7bab399eea9a016e8d2db1e6b4843f5d
StevePro7 <steveprozune@hotmail.com>
2018-07-20 09:06:52 +0100

    Additional screens

---
+++ 3Dcity.XNA/3Dcity.XNA.Library/Common/Screens/TestScreen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using WindowsGame.Common.Static;
+using WindowsGame.Define.Interfaces;
+using Microsoft.Xna.Framework;
+
+namespace WindowsGame.Common.Screens
+{
+	public class TestScreen : BaseScreen, IScreen
+	{
+		public override void Initialize()
+		{
+			base.Initialize();
+		}
+
+		public override void LoadContent()
+		{
+			base.LoadContent();
+		}
+
+		public Int32 Update(GameTime gameTime)
+		{
+			return (Int32)ScreenType.Test;
+		}
+
+		public override void Draw()
+		{
+			base.Draw();
+		}
+
+	}
+}
