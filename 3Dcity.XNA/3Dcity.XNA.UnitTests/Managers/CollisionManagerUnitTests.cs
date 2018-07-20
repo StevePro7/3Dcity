ca612b365653636ae581f71b984849f1c1a1fbc5
StevePro7 <steveprozune@hotmail.com>
2018-07-20 08:12:00 +0100

    Update test projects

---
+++ 3Dcity.XNA/3Dcity.XNA.UnitTests/Managers/CollisionManagerUnitTests.cs
@@ -0,0 +1,30 @@
+﻿using WindowsGame.Common.Managers;
+using NUnit.Framework;
+
+namespace WindowsGame.UnitTests.Managers
+{
+	[TestFixture]
+	public class CollisionManagerUnitTests : BaseUnitTests
+	{
+		[SetUp]
+		public new void SetUp()
+		{
+			// System under test.
+			CollisionManager = new CollisionManager();
+			base.SetUp();
+		}
+
+		[Test]
+		public void CheckEnemyCollisionExitTest()
+		{
+			Assert.That(1, Is.EqualTo(1));
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			CollisionManager = null;
+		}
+
+	}
+}
