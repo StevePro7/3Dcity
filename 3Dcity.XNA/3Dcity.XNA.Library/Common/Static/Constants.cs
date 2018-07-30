d33b60a65de712fddcafbc0c98495d3cd73c401e
StevePro7 <steveprozune@hotmail.com>
2018-07-30 21:38:17 +0100

    Revert full screen

--- 3Dcity.XNA/3Dcity.XNA.Library/Common/Static/Constants.cs
+++ 3Dcity.XNA/3Dcity.XNA.Library/Common/Static/Constants.cs
@@ -21,12 +21,12 @@ namespace WindowsGame.Common.Static
 		// Custom data.
 #if WINDOWS
 		public const Platform Platform = Static.Platform.Desktop;
-		public const Boolean IsFullScreen = true;
+		public const Boolean IsFullScreen = false;
 		public const Boolean IsMouseVisible = true;
 		public const UInt16 ScreenWide = 800;
 		public const UInt16 ScreenHigh = 480;
 
-		public const Boolean UseExposed = false;
+		public const Boolean UseExposed = true;
 		public const UInt16 ExposeWide = 800;
 		public const UInt16 ExposeHigh = 480;
 
