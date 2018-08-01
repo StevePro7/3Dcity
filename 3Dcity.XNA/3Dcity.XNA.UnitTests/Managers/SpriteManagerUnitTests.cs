b87d78460688770ed91795576b2b62dbab0435f4
StevePro7 <steveprozune@hotmail.com>
2018-08-01 23:01:38 +0100

    Add unit tests

---
+++ 3Dcity.XNA/3Dcity.XNA.UnitTests/Managers/SpriteManagerUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using WindowsGame.Common.Managers;
+using NUnit.Framework;
+namespace WindowsGame.UnitTests.Managers
+{
+	[TestFixture]
+	public class SpriteManagerUnitTests : BaseUnitTests
+	{
+		[SetUp]
+		public new void SetUp()
+		{
+			// System under test.
+			ControlManager = new ControlManager();
+			base.SetUp();
+		}
+
+		[Test]
+		public void MyTest()
+		{
+			Assert.That(1, Is.EqualTo(1));
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			CollisionManager = null;
+		}
+	}
+}
