96d09d60a748017b27a2817b383cd51657cbfc27
StevePro7 <steveprozune@hotmail.com>
2018-09-17 08:14:00 +0100

    Archive event screen test

--- 3Dcity.XNA/3Dcity.XNA.Library/Common/Screens/ReadyScreen.cs
+++ 3Dcity.XNA/3Dcity.XNA.Library/Common/Screens/ReadyScreen.cs
@@ -22,20 +22,18 @@ namespace WindowsGame.Common.Screens
 		{
 			base.Update(gameTime);
 
-			MyGame.Manager.EventManager.ClearEvents();
+			// Events
+			//MyGame.Manager.EventManager.ClearEvents();
 
+			// Move target unconditionally.
 			Single horz = MyGame.Manager.InputManager.Horizontal();
 			Single vert = MyGame.Manager.InputManager.Vertical();
 			MyGame.Manager.SpriteManager.SetMovement(horz, vert);
+			MyGame.Manager.SpriteManager.Update(gameTime);
 
-			MoveTarget(gameTime);
+			// Events.
+			//MyGame.Manager.EventManager.ProcessEvents(gameTime);
 
-
-			//MyGame.Manager.EventManager.Update(gameTime);
-			//MyGame.Manager.ExplosionManager.Update(gameTime);
-			//MyGame.Manager.BulletManager.Update(gameTime);
-
-			MyGame.Manager.EventManager.ProcessEvents(gameTime);
 			MyGame.Manager.ScoreManager.Update(gameTime);
 			return (Int32) CurrScreen;
 		}
@@ -52,24 +50,6 @@ namespace WindowsGame.Common.Screens
 
 			// Text data last!
 			MyGame.Manager.TextManager.Draw(TextDataList);
-		}
-
-		private static void MoveTarget(GameTime gameTime)
-		{
-			Vector2 largeTargetPosBEF = MyGame.Manager.SpriteManager.LargeTarget.Position;
-			Vector2 smallTargetPosBEF = MyGame.Manager.SpriteManager.SmallTarget.Position;
-			MyGame.Manager.SpriteManager.Update(gameTime);
-			Vector2 largeTargetPosAFT = MyGame.Manager.SpriteManager.LargeTarget.Position;
-			Vector2 smallTargetPosAFT = MyGame.Manager.SpriteManager.SmallTarget.Position;
-
-			if (largeTargetPosBEF != largeTargetPosAFT)
-			{
-				MyGame.Manager.EventManager.AddLargeTargetMoveEvent(largeTargetPosAFT);
-			}
-			if (smallTargetPosBEF != smallTargetPosAFT)
-			{
-				MyGame.Manager.EventManager.AddSmallTargetMoveEvent(smallTargetPosAFT);
-			}
 		}
 
 	}
