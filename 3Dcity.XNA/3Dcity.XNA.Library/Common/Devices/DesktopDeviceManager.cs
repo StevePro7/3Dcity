bd905241478febd8b81edc9add5ca44ed8f91331
StevePro7 <steveprozune@hotmail.com>
2018-10-02 08:25:46 +0100

    Update device manager

---
+++ 3Dcity.XNA/3Dcity.XNA.Library/Common/Devices/DesktopDeviceManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WindowsGame.Common.Interfaces;
+
+namespace WindowsGame.Common.Devices
+{
+	public class DesktopDeviceManager : IDeviceManager
+	{
+		public void Initialize()
+		{
+			BuildVersion = "1.0.0.";
+
+#if ANDROID
+			BuildVersion = "1.0.0";
+#endif
+#if IOS
+			BuildVersion = "1.0.0";
+#endif
+		}
+
+		public String BuildVersion { get; private set; }
+	}
+}
---
+++ 3Dcity.XNA/3Dcity.XNA.Library/Common/Interfaces/IDeviceManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WindowsGame.Common.Interfaces
+{
+	public interface IDeviceManager
+	{
+		void Initialize();
+
+		String BuildVersion { get; }
+	}
+}
