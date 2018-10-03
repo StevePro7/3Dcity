ba53944fac0750557822057a679a7ab426f3eacf
StevePro7 <steveprozune@hotmail.com>
2018-10-03 22:28:52 +0100

    Full screen in Relase mode on Windows configuration

--- 3Dcity.XNA/3Dcity.XNA.Library/Common/Static/Constants.cs
+++ 3Dcity.XNA/3Dcity.XNA.Library/Common/Static/Constants.cs
@@ -107,20 +107,21 @@ namespace WindowsGame.Common.Static
 //        public const Byte GameOffsetY = 60;// 0
 //#endif
 
+// Windows release mode - this works full screen but mouse does not detect correct screen location!
 #if WINDOWS && !DEBUG
 		public const PlatformType PlatformType = Static.PlatformType.Desktop;
 		public const Boolean IsFullScreen = true;
 		public const Boolean IsMouseVisible = true;
 		public const UInt16 ScreenWide = 800;
-		public const UInt16 ScreenHigh = 600;
+		public const UInt16 ScreenHigh = 480;
 		public const UInt16 GridHeight = 240;
 
-		public const Boolean UseExposed = true;
+		public const Boolean UseExposed = false;
 		public const UInt16 ExposeWide = 800;
-		public const UInt16 ExposeHigh = 600;
+		public const UInt16 ExposeHigh = 480;
 
 		public const Byte GameOffsetX = 0;
-		public const Byte GameOffsetY = 60;
+		public const Byte GameOffsetY = 0;
 #endif
 
 		// IMPORTANT
