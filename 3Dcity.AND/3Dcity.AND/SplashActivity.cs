f2076bb05473a4c415ffe824dc323cef8c4be631
StevePro7 <steveprozune@hotmail.com>
2019-10-29 12:12:00 +0000

    no message

--- 3Dcity.AND/3Dcity.AND/SplashActivity.cs
+++ 3Dcity.AND/3Dcity.AND/SplashActivity.cs
@@ -5,20 +5,20 @@ using Android.OS;
 // http://developer.xamarin.com/guides/android/user_interface/creating_a_splash_screen
 namespace WindowsGame
 {
-	[Activity(Label = "3D City"
-		, MainLauncher = true
-		, NoHistory = true
-		, Icon = "@drawable/icon"
-		, Theme = "@style/Theme.Splash"
-		, AlwaysRetainTaskState = true
-		, ScreenOrientation = ScreenOrientation.SensorLandscape)]
-	public class SplashActivity : Activity
-	{
-		protected override void OnCreate(Bundle bundle)
-		{
-			base.OnCreate(bundle);
-			StartActivity(typeof(GameActivity));
-		}
-	}
+    [Activity(Label = "3D City"
+        , MainLauncher = true
+        , NoHistory = true
+        , Icon = "@drawable/icon"
+        , Theme = "@style/Theme.Splash"
+        , AlwaysRetainTaskState = true
+        , ScreenOrientation = ScreenOrientation.SensorLandscape)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            StartActivity(typeof(GameActivity));
+        }
+    }
 
 }
